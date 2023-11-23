d4ca64730ca2fac0d28d8c5e31843075f5dd59db
Krushi1902 <88939761+Krushi1902@users.noreply.github.com>
2023-11-23 13:15:29 +0530

    Update darkmode.js krushi

--- darkmode.js
+++ darkmode.js
@@ -6,10 +6,24 @@ function darkmode() {
     element.classList.toggle('dark-mode', !wasDarkmode);
     const questionElement = document.getElementById('question');
     questionElement.style.color = !wasDarkmode ? 'white' : 'black';
+
+    const progressTextElement = document.getElementById('progressText');
+    progressTextElement.style.color = !wasDarkmode ? 'white' : 'black';
+    const hudPrefixElement = document.getElementsByClassName('hud-prefix');
+    for (let i = 0; i < hudPrefixElement.length; i++) {
+        hudPrefixElement[i].style.color = !wasDarkmode ? 'white' : 'black';
+    }
 }
 
 function onload() {
     document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
     const questionElement = document.getElementById('question');
     questionElement.style.color = localStorage.getItem('darkmode') === 'true' ? 'white' : 'black';
+
+    const progressTextElement = document.getElementById('progressText');
+    progressTextElement.style.color = localStorage.getItem('darkmode') === 'true' ? 'white' : 'black';
+    const hudPrefixElement = document.getElementsByClassName('hud-prefix');
+    for (let i = 0; i < hudPrefixElement.length; i++) {
+        hudPrefixElement[i].style.color = localStorage.getItem('darkmode') === 'true' ? 'white' : 'black';
+    }
 }
