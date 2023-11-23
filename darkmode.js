ea24c6c7d4bc6913df5a9fd2d594f5b41d590615
unset <unset>
2023-11-23 14:41:31 +0530

    after pres

--- darkmode.js
+++ darkmode.js
@@ -1,10 +1,29 @@
 function darkmode() {
     const wasDarkmode = localStorage.getItem('darkmode') === 'true';
     localStorage.setItem('darkmode', !wasDarkmode);
+
     const element = document.body;
     element.classList.toggle('dark-mode', !wasDarkmode);
+    const questionElement = document.getElementById('question');
+    questionElement.style.color = !wasDarkmode ? 'white' : 'black';
+
+    const progressTextElement = document.getElementById('progressText');
+    progressTextElement.style.color = !wasDarkmode ? 'white' : 'black';
+    const hudPrefixElement = document.getElementsByClassName('hud-prefix');
+    for (let i = 0; i < hudPrefixElement.length; i++) {
+        hudPrefixElement[i].style.color = !wasDarkmode ? 'white' : 'black';
+    }
 }
 
 function onload() {
     document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+    const questionElement = document.getElementById('question');
+    questionElement.style.color = localStorage.getItem('darkmode') === 'true' ? 'white' : 'black';
+
+    const progressTextElement = document.getElementById('progressText');
+    progressTextElement.style.color = localStorage.getItem('darkmode') === 'true' ? 'white' : 'black';
+    const hudPrefixElement = document.getElementsByClassName('hud-prefix');
+    for (let i = 0; i < hudPrefixElement.length; i++) {
+        hudPrefixElement[i].style.color = localStorage.getItem('darkmode') === 'true' ? 'white' : 'black';
+    }
 }
