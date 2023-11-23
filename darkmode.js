bec3337fb3d1c608664059914b5c872bd8ca857a
Krushi1902 <88939761+Krushi1902@users.noreply.github.com>
2023-11-23 13:05:56 +0530

    Update darkmode.js krushi

--- darkmode.js
+++ darkmode.js
@@ -1,10 +1,15 @@
 function darkmode() {
     const wasDarkmode = localStorage.getItem('darkmode') === 'true';
     localStorage.setItem('darkmode', !wasDarkmode);
+
     const element = document.body;
     element.classList.toggle('dark-mode', !wasDarkmode);
+    const questionElement = document.getElementById('question');
+    questionElement.style.color = !wasDarkmode ? 'white' : 'black';
 }
 
 function onload() {
     document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+    const questionElement = document.getElementById('question');
+    questionElement.style.color = localStorage.getItem('darkmode') === 'true' ? 'white' : 'black';
 }
